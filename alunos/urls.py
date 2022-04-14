de923935213d37cf1b8ed8469e8c828dc7eb85a1
Waleson Melo <walesonmelo23@gmail.com>
2022-04-14 01:47:59 -0400

    Rotas cadastrar alunos adicionada

--- alunos/urls.py
+++ alunos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import IndexAlunos
+from .views import *
 
 
 urlpatterns = [
     path('', IndexAlunos.as_view(), name='index_aluno'),
+    path('cadastrar/', CadastrarAluno.as_view(), name='cadastrar_aluno'),
 ]
--- alunos/views.py
+++ alunos/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class IndexAlunos(TemplateView):
     template_name = 'paginas/index_aluno.html'
+
+class CadastrarAluno(TemplateView):
+    template_name = 'paginas/cadastrar_aluno.html'
