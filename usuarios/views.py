436ea3d5eee79ae8d57e3e26a203e4d0173ffbee
Waleson Melo <walesonmelo23@gmail.com>
2022-04-16 17:47:49 -0400

    Update App Usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from braces.views import GroupRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
+
+from .forms import UsuarioForm
 
 
 # Create your views here.
@@ -13,10 +16,19 @@ class IndexUsuarios(GroupRequiredMixin, LoginRequiredMixin, TemplateView):
     template_name = 'paginas/index_usuario.html'
 
 
-class CadastrarUsuario(GroupRequiredMixin, LoginRequiredMixin, TemplateView):
+class CadastrarUsuario(GroupRequiredMixin, LoginRequiredMixin, CreateView):
     login_url = reverse_lazy('usuarios-login')
     group_required = u'Administrador'
-    template_name = 'paginas/cadastrar_usuario.html'
+    template_name = 'usuarios/form.html'
+    success_url = reverse_lazy('usuarios-listar')
+    form_class = UsuarioForm
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context['titulo'] = 'Cadastrar Usuário'
+
+        return context
 
 
 class AlterarUsuario(GroupRequiredMixin, LoginRequiredMixin, TemplateView):
